5b375301966bdfb42261f69af62c4855aa9326a2
tims_rayku <tims@rayku.net>
2018-07-02 16:04:10 +0800

    尝试第二版

--- api-demo.js
+++ api-demo.js
@@ -7,27 +7,37 @@ var protocol_headers = require('./v2ray.com/core/common/protocol/headers_pb')
 var serial = require('./v2ray.com/core/common/serial/typed_message_pb')
 var vmess = require('./v2ray.com/core/proxy/vmess/account_pb')
 
-var hsClient = new services.HandlerServiceClient('127.0.0.1:10085', grpc.credentials.createInsecure())
+var hsClient = new services.HandlerServiceClient('127.0.0.1:1088', grpc.credentials.createInsecure())
 
-var vAccount = new serial.TypedMessage(new vmess.Account({
-    Id: '4c5630a1-0a09-987f-3f04-58f7bbfc2773',
-    AlterId: 32,
-    SecuritySettings: new protocol_headers.SecurityConfig({
-        Type: protocol_headers.SecurityType.AUTO
-    })
-}))
 
-hsClient.AlterInbound(new command.AlterInboundRequest({
-    Tag: 'proxy',
-    Operation: new serial.TypedMessage(new command.AddUserOperation({
-        User: new protocol_user.User({
-            Level: 0,
-            Email: 't@t.tt',
-            Account: vAccount
-        })
-    }))
-}), function(err, resp){
+var securitySettings = new protocol_headers.SecurityConfig()
+securitySettings.setType(protocol_headers.SecurityType.AUTO)
+
+var v2Account = new vmess.Account()
+v2Account.setId('d6f5e396-cccc-4e45-b0eb-a953105cc262')
+v2Account.setAlterId(32)
+v2Account.setSecuritySettings(securitySettings)
+
+var v2AccountTypedMessage = new serial.TypedMessage()
+v2AccountTypedMessage.setValue(v2Account)
+
+var v2User = new protocol_user.User()
+v2User.setLevel(1)
+v2User.setEmail('tims@rayku.net')
+v2User.setAccuont(v2AccountTypedMessage)
+
+var v2AddUserOperation = new command.AddUserOperation()
+v2AddUserOperation.setUser(v2User)
+var v2AddUserOperationTypedMessage = new serial.TypedMessage()
+v2AddUserOperationTypedMessage.setValue(v2AddUserOperation)
+
+var v2AlterInboundRequest = new command.AlterInboundRequest()
+v2AlterInboundRequest.setTag('tcp')
+v2AlterInboundRequest.setOperation(v2AddUserOperationTypedMessage)
+
+hsClient.alterInbound(v2AlterInboundRequest, function(err, resp){
     if (err) {
+        console.log(err)
         return
     }
     console.log('Response:', resp.getMessage());
